c5b37c3fa26da3e3c6a4dfa04dc0f932ba03aab5
captaindmitro <d.druzhkoff@gmail.com>
2022-08-12 15:23:17 +0300

    Fix: ViewModel saves state

--- app/src/main/java/ru/captaindmitro/warehouseapp/domain/models/Product.kt
+++ app/src/main/java/ru/captaindmitro/warehouseapp/domain/models/Product.kt
@@ -1,5 +1,8 @@
 package ru.captaindmitro.warehouseapp.domain.models
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class Product(
     val code: Int,
     val barcode: String,
@@ -9,4 +12,41 @@ data class Product(
     val remain: Double,
     val type: Int?,
     val alc: Double?
-)
+) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readDouble(),
+        parcel.readDouble(),
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readValue(Double::class.java.classLoader) as? Double
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(code)
+        parcel.writeString(barcode)
+        parcel.writeString(name)
+        parcel.writeString(checkText)
+        parcel.writeDouble(price)
+        parcel.writeDouble(remain)
+        parcel.writeValue(type)
+        parcel.writeValue(alc)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Product> {
+        override fun createFromParcel(parcel: Parcel): Product {
+            return Product(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Product?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
--- app/src/main/java/ru/captaindmitro/warehouseapp/presentation/viewmodels/SharedViewModel.kt
+++ app/src/main/java/ru/captaindmitro/warehouseapp/presentation/viewmodels/SharedViewModel.kt
@@ -1,10 +1,12 @@
 package ru.captaindmitro.warehouseapp.presentation.viewmodels
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -14,16 +16,20 @@ import ru.captaindmitro.warehouseapp.domain.usecases.GetProductsUseCase
 import ru.captaindmitro.warehouseapp.presentation.common.UiState
 import javax.inject.Inject
 
+private const val SELECTED_PRODUCT_KEY = "SELECTED_PRODUCT"
+
 @HiltViewModel
 class SharedViewModel @Inject constructor(
+    private val state: SavedStateHandle,
     private val getProductsUseCase: GetProductsUseCase
 ) : ViewModel() {
 
     private val _products: MutableStateFlow<UiState<List<Product>>> = MutableStateFlow(UiState.Empty)
     val products = _products.asStateFlow()
 
-    private val _selectedProduct: MutableStateFlow<Product?> = MutableStateFlow(null)
-    val selectedProduct = _selectedProduct.asStateFlow()
+//    private val _selectedProduct: MutableStateFlow<Product?> = MutableStateFlow(null)
+//    val selectedProduct = _selectedProduct.asStateFlow()
+    val selectedProduct: StateFlow<Product?> = state.getStateFlow(SELECTED_PRODUCT_KEY, null)
 
     init {
         getProducts()
@@ -46,7 +52,8 @@ class SharedViewModel @Inject constructor(
     }
 
     fun setSelectedProduct(position: Int) {
-        _selectedProduct.value = (products.value as UiState.Success).data[position]
+//        _selectedProduct.value = (products.value as UiState.Success).data[position]
+        state[SELECTED_PRODUCT_KEY] = (products.value as UiState.Success).data[position]
     }
 
 }
